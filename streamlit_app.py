97e7e2b3191dd3a043e2b1ff0c62941068d2dd90
J <142761409+J-coder118@users.noreply.github.com>
2023-11-15 20:50:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,104 @@
-import altair as alt
-import numpy as np
-import pandas as pd
+import csv
+import os
+from supabase.client import Client, create_client
+import openai
+import httpx
+import requests
 import streamlit as st
+from dotenv import load_dotenv
 
-"""
-# Welcome to Streamlit!
+load_dotenv()
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+supabase_url = os.getenv("SUPABASE_URL")
+supabase_key = os.getenv("SUPABASE_KEY")
+openai_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = openai_key 
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+function_name = "match_documents"
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+supabase = Client(supabase_url, supabase_key)
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+st.title("chat")
+st.caption("Please ask what you want.")
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+question = st.text_input("Question")
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+if st.button("Get Answer"):
+    response = openai.Embedding.create(model="text-embedding-ada-002", input=question)
+    data = response["data"]
+
+    embedding_query = [embedding for embedding in data]
+
+    print("LOADING...")
+
+    print("sfddsfds")
+    similarity_threshold = 0.79
+    match_count = 10
+    model_engine = "gpt-4"
+    payload = {
+        "query_embedding": embedding_query[0]["embedding"],
+        "match_count": match_count,
+        "match_threshold": 0.8,
+    }
+
+    response = supabase.rpc(function_name, payload).execute()
+
+    data = response.data
+    response_data_0 = data[0]["description"]
+    response_data_1 = data[1]["description"]
+    response_data_2 = data[2]["description"]
+    response_data_3 = data[3]["description"]
+    response_data_4 = data[4]["description"]
+    response_data_5 = data[5]["description"]
+    response_data_6 = data[6]["description"]
+    response_data_7 = data[7]["description"]
+    response_data_8 = data[8]["description"]
+    response_data_9 = data[9]["description"]
+
+    prompt = [
+        {
+            "role": "system",
+            "content": "You are an AI assistant that provides helpful answers.",
+        },
+        {
+            "role": "user",
+            "content": f"""
+        Just rewrite this content in Russian
+        {response_data_0+
+         response_data_1+
+         response_data_2+
+         response_data_3+
+         response_data_4+
+         response_data_5+
+         response_data_6+
+         response_data_7+
+         response_data_8+
+         response_data_9
+         }
+        """,
+        },
+    ]
+
+    response = openai.ChatCompletion.create(
+        model=model_engine,
+        messages=prompt,
+        max_tokens=2000,
+        n=1,
+        stop=None,
+        temperature=0.8,
+    )
+
+    ai_response = response.choices[0].message["content"]
+
+    st.write("AI Answer:", ai_response)
+
+    if isinstance(response_data_0, list) and len(response_data_0) > 0:
+        first_result = response_data_0[0]
+        description = first_result.get("description")
+        if description:
+            print(description)
+        else:
+            print("Description not found in the response.")
+    else:
+        print("No response data or empty response.")
